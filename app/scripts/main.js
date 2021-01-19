508a489f796f75b2960fc0555a2bd55eeee69adb
ZXHM <zxhm001@126.com>
2021-01-20 01:45:48 +0800

    feat:框架基本搭建完成

---
+++ app/scripts/main.js
@@ -0,0 +1,13 @@
+function init(){
+    window.viewer = new Cesium.Viewer('cesium_container');
+    var url = 'http://localhost:8090/iserver/services/3D-GuanXianXiangMuCeShi/rest/realspace';
+    var promise = viewer.scene.open(url);
+    Cesium.when(promise,function(layer){
+        console.log(layers);
+    });
+}
+
+if (typeof Cesium !== 'undefined') {
+    window.startupCalled = true;
+    init(Cesium);
+}
