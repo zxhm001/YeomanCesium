dd4071f39c17fa1f628a3aa78fd5fa746c5ce696
ZXHM <zxhm001@126.com>
2021-01-20 02:23:40 +0800

    feat:切换到上海的数据

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,6 @@
 function init(){
     window.viewer = new Cesium.Viewer('cesium_container');
-    var url = 'http://localhost:8090/iserver/services/3D-GuanXianXiangMuCeShi/rest/realspace';
+    var url = 'http://localhost:8090/iserver/services/3D-SHAB/rest/realspace';
     var promise = viewer.scene.open(url);
     Cesium.when(promise,function(layer){
         console.log(layers);
