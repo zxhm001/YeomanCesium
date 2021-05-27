a59966261d6f2c9e9ab288f8ae9d8cbe4b8c9e3c
ZXHM <zxhm001@126.com>
2021-05-27 16:27:45 +0800

    fix:使用外网地址方便多电脑调试

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -1,4 +1,4 @@
-var API_ROOT = 'http://localhost:53785';
+var API_ROOT = 'http://192.168.1.28:53785';
 var Uniptt_POC_URL = 'ws://127.0.0.1:1338/poc'
 var Uniptt_LOC_URL = 'ws://112.74.167.167:1337/loc'
 
